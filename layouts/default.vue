ae651dcd196100d4c0b85df2a43124043c49e49e
adrianvanladrillano <64422461+adrianvanladrillano@users.noreply.github.com>
2021-11-21 02:10:49 +0800

    Initial Commit
    Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-app-bar color="transparent" elevate-on-scroll absolute dark>
+      <img
+        height="50"
+        contain
+        src="https://images.squarespace-cdn.com/content/5134cbefe4b0c6fb04df8065/1540481377494-6F0ZRE16VTIMGIFPVW7K/squarespace-logo-horizontal-black.jpg?format=1000w&content-type=image%2Fjpeg"
+      />
+      <v-spacer></v-spacer>
+      <v-btn text>About</v-btn>
+      <v-btn text>Works</v-btn>
+      <v-btn text>Services</v-btn>
+      <v-btn text>Contact</v-btn>
+    </v-app-bar>
+    <v-main>
+      <Nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <v-card
+      style="
+        padding-left: 15vw;
+        background: url('https://cdn.wallpapersafari.com/85/71/J3Zxem.jpg');
+      "
+    >
+      <v-row align="center" justify="center" style="height: 101vh">
+        <v-col>
+          <v-card flat width="30vw" color="transparent" dark>
+            <v-card-title>
+              <h1 class="font-weight-bold display-2">
+                Hello,
+                <br />I'm Adrian Van Ladrillano
+              </h1>
+            </v-card-title>
+            <v-card-text>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+              dolores eveniet fuga accusantium asperiores adipisci aperiam eaque
+              est odit! Ducimus voluptas quas dolor laborum optio repudiandae
+              pariatur quae architecto autem.
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn>asdasd</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <v-col></v-col>
+      </v-row>
+    </v-card>
+
+    <v-card>
+      <v-row align="center" justify="center" style="height: 101vh">
+        <v-col>
+          <v-img
+            src="https://cdn.dribbble.com/users/791530/screenshots/6827794/clip-illustration-style-icons8_4x.png?compress=1&resize=400x300"
+          ></v-img>
+        </v-col>
+
+        <v-col>
+          <v-card flat width="30vw" color="transparent">
+            <v-card-title>
+              <h1 class="font-weight-bold display-2">
+                Hello,
+                <br />I'm Adrian Van Ladrillano
+              </h1>
+            </v-card-title>
+            <v-card-text>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+              dolores eveniet fuga accusantium asperiores adipisci aperiam eaque
+              est odit! Ducimus voluptas quas dolor laborum optio repudiandae
+              pariatur quae architecto autem.
+            </v-card-text>
+
+            <v-card-text>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+              dolores eveniet fuga accusantium asperiores adipisci aperiam eaque
+              est odit! Ducimus voluptas quas dolor laborum optio repudiandae
+              pariatur quae architecto autem.
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn>asdasd</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
+  </div>
+</template>
